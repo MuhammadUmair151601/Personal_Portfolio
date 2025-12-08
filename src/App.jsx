bd549ff3a0edd17cf47d9ecd6b78cb1ca16b2421
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2025-12-08 18:11:50 +0500

    all component files imported to App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Education from './components/Education/Education'
+import Experience from './components/Experience/Experience'
+import Navbar from './components/Navbar/Navbar'
+import Work from './components/Work/Work'
 
 const App = () => {
   return (
-    <div className='text-5xl text-red-600'>App</div>
+    <div></div>
   )
 }
 
