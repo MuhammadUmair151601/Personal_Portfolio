80becb8d4ef536dc026743021c1a9e72751b8fbf
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-28 16:27:05 +0500

    dark mode code done in main.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import Navbar from './components/Navbar'
 const App = () => {
+  const [darkMode, setdarkMode] = useState(true)
+  useEffect(()=>{
+    AOS.init({
+      duration : 1000,
+      once : false,
+      offset : 100
+    })
+    document.documentElement.classList.add('dark')
+  }, [])
+  const toggleDarkMode = ()=>{
+    const newMode = !darkMode;
+    setDarkMode(newMode);
+    document.documentElement.classList.toggle('dark');
+  }
   return (
-    <div>App</div>
+    <div className={darkmode 
+      ?'bg-linear-to-br from-gray-900 via-[#0d182e] min-h-screen'
+      :'bg-linear-to-br from-gray-50 to-blue-50 min-h-screen'
+    }>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+    </div>
   )
 }
 
