61b7b1495b0f4b979525095f2c083af1d966a0a5
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-01-28 12:07:22 +0500

    lucide framer motion and aos packages installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,8 @@
 import React from 'react'
-import About from './components/About/About'
-import Contact from './components/Contact/Contact'
-import Education from './components/Education/Education'
-import Experience from './components/Experience/Experience'
-import Navbar from './components/Navbar/Navbar'
-import Work from './components/Work/Work'
-import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div className='bg-blue-950 min-h-screen'>
-      <div></div>
-      <div>
-        <Navbar/>
-      </div>
-    </div>
+    <div>App</div>
   )
 }
 
