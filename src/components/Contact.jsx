386deab8b0af409079c8ead8c44f17ef28387ba4
MuhammadUmair151601 <157686720+MuhammadUmair151601@users.noreply.github.com>
2026-02-20 21:17:02 +0500

    contact half portion done

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,10 +1,83 @@
+import {
+  MapPin,
+  Mail,
+  Phone,
+  BadgeCheck,
+  Github,
+  Linkedin,
+  Facebook,
+} from "lucide-react";
+
 const Contact = () => {
   return (
     <section
       id="contact"
-      className="h-screen bg-blue-950 text-white flex justify-center items-center"
+      className="h-full bg-gray-900 text-white flex flex-wrap"
     >
-      <h1 className="text-4xl">This is Contact Page</h1>
+      <div className="p-20 h-10 w-full">
+        <h1 className="text-3xl font-bold text-center">
+          Get in <span className="text-orange-400">Touch</span>
+        </h1>
+        <p className="text-center">
+          Have a project in mind or want to collaborate? Let's talk!
+        </p>
+      </div>
+      <div className="flex w-full">
+        <div className="w-1/2 bg-amber-800"></div>
+        <div className=" w-1/2 ">
+          <div className="w-full h-1/6 p-10">
+            <div className="flex gap-5 text-2xl font-bold justify-start items-center">
+              <MapPin size={30} /> <h1>Location</h1>
+            </div>
+            <h1 className="text-lg  ml-12">Peshawar,Pakistan.</h1>
+          </div>
+          <div className="w-full h-1/6 p-10">
+            <div className="flex gap-5 text-2xl font-bold justify-start items-center">
+              <Mail size={30} /> <h1>Email</h1>
+            </div>
+            <h1 className="text-lg  ml-12">muhammadumair4105@gmail.com</h1>
+          </div>
+          <div className="w-full h-1/6 p-10">
+            <div className="flex gap-5 text-2xl font-bold justify-start items-center">
+              <Phone size={30} /> <h1>Phone</h1>
+            </div>
+            <h1 className="text-lg  ml-12">03038951047</h1>
+          </div>
+          <div className="w-full  h-1/6 p-8">
+            <div className="flex gap-5 text-2xl font-bold justify-start items-center ">
+              <BadgeCheck size={30} /> <h1>Follow me on</h1>
+            </div>
+            <div className="flex gap-4 text-white ml-10 pt-3">
+              <a
+                href="https://github.com/MuhammadUmair151601"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition hover:text-black hover:bg-orange-400 rounded-[50%] h-10 w-10 flex items-center justify-center shadow-[0_0_25px_#f97316]"
+              >
+                <Github size={28} />
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/muhammad-umair-a7b10a2b2/"
+                target="_blank"
+                rel="noopener noreferrer"
+               className="transition hover:text-black hover:bg-orange-400 rounded-[50%] h-10 w-10 flex items-center justify-center shadow-[0_0_25px_#f97316]"
+              >
+                <Linkedin size={28} />
+              </a>
+
+              <a
+                href="https://www.facebook.com/mumair.umair.9022"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="transition hover:text-black hover:bg-orange-400 rounded-[50%] h-10 w-10 flex items-center justify-center shadow-[0_0_25px_#f97316]"
+              >
+                <Facebook size={30} />
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
